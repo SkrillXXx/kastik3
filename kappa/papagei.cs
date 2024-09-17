051f0006ce39ac227d13e58971e4ecca439c7a7e
Студент <student@ad.anosov.ru>
2024-09-17 09:41:49 +0500

    kap

--- kappa/papagei.cs
+++ kappa/papagei.cs
@@ -1,1 +1,20 @@
-kosty_bulat
+using System;
+namespace papagei
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cherty = Convert.ToInt32(Console.ReadLine());
+            natural(cherty);
+        }
+        static void natural(int cherty)
+        {
+            for (int i = 0; i < cherty; i++)
+            {
+                Console.Write("-");
+            }
+
+        }
+    }
+}
